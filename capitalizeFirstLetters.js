a07702fac4ad940538867ddd3808ccf8869e2c0c
Emilie Anglade <anglade.em@gmail.com>
2020-12-14 12:29:39 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,11 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+  return input.length > 0 ? 
+    input.split(' ').map(word=> word[0].toUpperCase() + word.slice(1)).join(' ') 
+    : '';
+}
+
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD')
