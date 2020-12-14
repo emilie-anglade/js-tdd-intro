f8d6cc49791e4f041919aa3e0272b96deb05b268
Emilie Anglade <anglade.em@gmail.com>
2020-12-14 22:43:08 +0100

    git tag step-3

---
+++ test/Rectangle.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const Rectangle = require('../Rectangle');
+
+
+describe('Rectangle', () => {
+  
+  it('isSquare should return true', () => {
+    const rectangle = new Rectangle(5, 5);
+    assert.strictEqual(rectangle.isSquare(), true);
+  });
+  it('isSquare should return false', () => {
+    const rectangle = new Rectangle(6, 5);
+    assert.strictEqual(rectangle.isSquare(), false);
+  });
+
+  it('getArea should return the area', () => {
+    const rectangle = new Rectangle(13, 7);
+    assert.strictEqual(rectangle.getArea(), 91);
+  });
+
+  it('getPerimeter should return the perimeter', () => {
+    const rectangle = new Rectangle(13, 7);
+    assert.strictEqual(rectangle.getPerimeter(), 40);
+  });
+});
