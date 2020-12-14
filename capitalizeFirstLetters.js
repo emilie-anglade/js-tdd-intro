4e7a9abb4df9949d585d209341dcd055ea0841a6
Emilie Anglade <anglade.em@gmail.com>
2020-12-14 21:21:17 +0100

    test runners

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -6,11 +6,6 @@ function capitalizeFirstLetters(input) {
     : '';
 }
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD')
-assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-assert.strictEqual(capitalizeFirstLetters(''), '');
+module.exports = capitalizeFirstLetters;
 
 
